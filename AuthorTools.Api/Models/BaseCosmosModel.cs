0bcd8afbc631fe564f11ff51403fc4523fefb9b0
Mat Howard <0x001a4@gmail.com>
2024-11-04 20:09:12 -0500

    Tiny cleanup

--- AuthorTools.Api/Models/BaseCosmosModel.cs
+++ AuthorTools.Api/Models/BaseCosmosModel.cs
@@ -1,10 +1,7 @@
-﻿using System.Text.Json.Serialization;
-
-namespace AuthorTools.Api.Models;
+﻿namespace AuthorTools.Api.Models;
 
 public abstract class BaseCosmosModel
 {
-    [JsonPropertyName("id")]
     public string? Id { get; set; }
     public string? PartitionKey { get; set; }
 }
