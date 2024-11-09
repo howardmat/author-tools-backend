614efbef4cb15a8aae6172bb1fb6cd3ee665d60d
Mat Howard <0x001a4@gmail.com>
2024-11-08 21:31:28 -0500

    Updated model

--- AuthorTools.Api/Models/Character.cs
+++ AuthorTools.Api/Models/Character.cs
@@ -2,6 +2,25 @@
 
 public class Character : BaseCosmosModel
 {
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
+    public string? Name { get; set; }
+    public string? Alias { get; set; }
+    public string? Archetype { get; set; }
+    public string? ImageUrl { get; set; }
+    public DateTimeOffset? BirthDate { get; set; }
+    public int? Age { get; set; }
+    public string? Profession { get; set; }
+    public string? LoveInterest { get; set; }
+    public string? Gender { get; set; }
+    public string? EyeColor { get; set; }
+    public string? HairType { get; set; }
+    public string? HairColor { get; set; }
+    public string? BodyShape { get; set; }
+    public string? PersonalTraits { get; set; }
+    public string? Abilities { get; set; }
+    public string? Strength { get; set; }
+    public string? Weakness { get; set; }
+    public string? FriendsAndFamily { get; set; }
+    public string? History { get; set; }
+    public string? FamilyHistory { get; set; }
+    public IEnumerable<CharacterRelationship> Relationships { get; set; } = [];
 }
---
+++ AuthorTools.Api/Models/CharacterRelationship.cs
@@ -0,0 +1,7 @@
+﻿namespace AuthorTools.Api.Models;
+
+public class CharacterRelationship
+{
+    public string? Name { get; set; }
+    public string? Relationship { get; set; }
+}
